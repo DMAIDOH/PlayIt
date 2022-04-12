2e01547633a61e56a594ef076913597608b00e3c
DMAIDOH <deborahmaidoh@gmail.com>
2022-04-12 11:40:33 -0400

    first commit

---
+++ app/src/main/java/com/example/playit/my_playlist.java
@@ -0,0 +1,14 @@
+package com.example.playit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class my_playlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_playlist);
+    }
+}
